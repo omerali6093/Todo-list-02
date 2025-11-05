bf85fe56c3fd2bfebcb064a9fafeff987cf1a985
Omer Ali <omerali200765@gmail.com>
2025-11-05 17:48:10 +0500

    todo warpper init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react"
+import TodoWrapper from "./components/TodoWrapper";
 
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="main-container w-1/2 mx-auto mt-10">
+      <h1 className="text-[30px] font-bold">My To-Do List</h1>
+      < TodoWrapper />
+    </div>
   )
 }
- 
 
 
-export default App
+
+export default App;
