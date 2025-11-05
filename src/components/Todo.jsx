5622298d4747fcf3e2d7a5fd819be3f53403cb12
Omer Ali <omerali200765@gmail.com>
2025-11-05 21:44:07 +0500

    todo input components

---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Todo () {
+    return (
+        <div className="todo-list">
+            <div className="todo-title">
+                <p>Schedule team meeting for discussion on the new project</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,21 @@
+
+
+function TodoInput () {
+    return (
+        <div className="add-task-input border border-gray-200 mt-6 p-7
+         shadow-[rgba(0,0,0,0.15)_1.95px_1.95px_2.6px] rounded-lg">
+            <h2 className="text-[18px] font-medium">Add New Task</h2>
+            <div className="input-section mt-3 flex items-center gap-2">
+                <input 
+                type="text" 
+                placeholder="Enter a new task...." 
+                className="border  border-gray-300 w-[780px] py-2 px-3 text-[16px] rounded-lg outline-none"/>
+                <button className="border px-5 py-2 rounded-lg bg-[#636ae8]
+                 text-white cursor-pointer ">
+                    Add Task</button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoInput
